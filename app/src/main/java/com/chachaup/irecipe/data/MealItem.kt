eb3cc92b7249de265aec89a0e34f5581c6d596e9
chacha <chachaerickjo@gmail.com>
2022-12-26 19:06:07 +0300

    ft - add retrofit and gson converter factory libraries ft - add network call interface ft - add data classes for meal response item

--- app/src/main/java/com/chachaup/irecipe/data/MealItem.kt
+++ app/src/main/java/com/chachaup/irecipe/data/MealItem.kt
@@ -1,7 +1,7 @@
 package com.chachaup.irecipe.data
 
-data class MealItem (
+data class MealItem(
     val id: Int,
     val name: String,
     val pictureUrl: String
-        )
+)
---
+++ app/src/main/java/com/chachaup/irecipe/network/MealInterface.kt
@@ -0,0 +1,39 @@
+package com.chachaup.irecipe.network
+
+import com.chachaup.irecipe.BuildConfig
+import com.chachaup.irecipe.data.MealItem
+import com.chachaup.irecipe.data.MealResponseItem
+import okhttp3.Call
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface MealInterface {
+    @GET("meals")
+    fun getMeals(
+        @Query("s") meal: String
+    ): MealResponseItem
+
+    companion object{
+        operator fun invoke(): MealInterface{
+            val interceptor: HttpLoggingInterceptor = HttpLoggingInterceptor().apply {
+                this.level = HttpLoggingInterceptor.Level.BODY
+            }
+
+            val client = OkHttpClient
+                .Builder()
+                .apply { this.addInterceptor(interceptor) }
+                .build()
+
+            val retrofit = Retrofit.Builder()
+                .baseUrl(BuildConfig.MDB_URL)
+                .client(client)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+            return retrofit.create(MealInterface::class.java)
+        }
+    }
+}
