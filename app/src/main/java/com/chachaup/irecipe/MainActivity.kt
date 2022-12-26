04a95ef2c93100b433d8881dcb41e71ee7c0b023
chacha <chachaerickjo@gmail.com>
2022-12-26 18:03:34 +0300

    Initial commit

---
+++ app/src/main/java/com/chachaup/irecipe/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.chachaup.irecipe
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import com.chachaup.irecipe.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val binding : ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        val appConfig = AppBarConfiguration(navController.graph)
+        val appBarConfiguration = AppBarConfiguration(topLevelDestinationIds = setOf(), fallbackOnNavigateUpListener = ::onSupportNavigateUp)
+//        binding.toolBar.setupWithNavController(navController, appConfig)
+        supportActionBar?.hide()
+    }
+
+}
