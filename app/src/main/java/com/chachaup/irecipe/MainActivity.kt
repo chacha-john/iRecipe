ac3b359bc83273a2fe9326d3a8045aea661775a7
chacha <chachaerickjo@gmail.com>
2023-01-24 10:31:24 +0300

    res - add icons for bottom navigation ft - add menu resource file bug - navigation from account setup does not save that one is vegetarian or not res - add layout for account setup ft - add bottom navigation view to activity minor fixes

--- app/src/main/java/com/chachaup/irecipe/MainActivity.kt
+++ app/src/main/java/com/chachaup/irecipe/MainActivity.kt
@@ -3,11 +3,15 @@ package com.chachaup.irecipe
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.chachaup.irecipe.databinding.ActivityMainBinding
+import com.chachaup.irecipe.ui.Meals
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationBarView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var appBarConfiguration: AppBarConfiguration
@@ -20,6 +24,32 @@ class MainActivity : AppCompatActivity() {
         appBarConfiguration = AppBarConfiguration(navController.graph)
         setupActionBarWithNavController(navController,appBarConfiguration)
         supportActionBar?.hide()
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        bottomNavigationView.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.nav_discover -> {
+                    // Handle discover navigation
+                    navController.navigate(R.id.meals)
+                    it.isChecked = true
+                    true
+                }
+                R.id.nav_tips -> {
+                    // Handle tips navigation
+                    navController.navigate(R.id.tips)
+                    it.isChecked = true
+                    true
+                }
+                R.id.nav_favorites -> {
+                    // Handle favorites navigation
+                    navController.navigate(R.id.favorites)
+                    it.isChecked = true
+                    true
+                }
+                else -> false
+            }
+        }
+
 
     }
 
--- app/src/main/java/com/chachaup/irecipe/ui/AccountSetup.kt
+++ app/src/main/java/com/chachaup/irecipe/ui/AccountSetup.kt
@@ -5,16 +5,36 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
 import com.chachaup.irecipe.R
+import com.chachaup.irecipe.databinding.FragmentAccountSetupBinding
 
 class AccountSetup : Fragment() {
+    private lateinit var binding: FragmentAccountSetupBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_account_setup, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_account_setup, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.apply {
+            // set click listener for both buttons
+            buttonNope.setOnClickListener {
+                // navigate to the next fragment
+                findNavController().navigate(R.id.action_accountSetup_to_meals)
+            }
+            buttonYes.setOnClickListener {
+                // navigate to the next fragment
+                findNavController().navigate(R.id.action_accountSetup_to_meals)
+            }
+        }
     }
 
 }
